311879214762a6406ccc813d39db852f03c26d7d
vrbsky <vrbsky@users.noreply.github.com>
2020-09-04 07:38:01 -0300

    add year filter, improved alerts, texts, organization
    - added data description
    - reshaped and moved sections
    - added check for excel max size
    - improved alerts
    - added year filter
    - hid table export section when some alerts are alerting
    - hid exported table - unhide on click (shortens app execution delay)
    - added section that explains why table export section
    is not available (due to alerts)

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,17 @@ from itertools import compress
 #from flask import send_file
 
 st.title("Crie sua tabela")
-#st.markdown('v0.3.0')
+#st.markdown('v0.4.0')
+st.markdown(
+"""### Tesouro Transparente
+#### Transferências Constitucionais para Municípios
+Parcela das receitas federais arrecadadas pela União é repassada aos Municípios. O rateio da receita proveniente da arrecadação de impostos entre os entes federados representa um mecanismo fundamental para amenizar as desigualdades regionais, na busca incessante de promover o equilíbrio sócio-econômico entre Estados.
+
+Cabe ao Tesouro Nacional, em cumprimento aos dispositivos constitucionais, efetuar as transferências desses recursos aos entes federados, nos prazos legalmente estabelecidos.
+
+Dentre as principais transferências da União para os Municípios, previstas na Constituição, destacam-se: o Fundo de Participação dos Estados e do Distrito Federal (FPE); o Fundo de Participação dos Municípios (FPM); o Fundo de Compensação pela Exportação de Produtos Industrializados - FPEX; o Fundo de Manutenção e Desenvolvimento da Educação Básica e de Valorização dos Profissionais da Educação - Fundeb; e o Imposto sobre a Propriedade Territorial Rural - ITR.
+
+Dados disponíveis nesta aplicação são referentes ao mês de março em anos 2017, 2018, 2019 e 2020.""")
 
 #@st.cache
 def get_UN_data():
@@ -64,27 +74,12 @@ except urllib.error.URLError as e:
 available_rows_columns = ['ano', 'uf', 'municipio', 'transferencia', 'item transferencia']
 available_variables = ['1o decendio', '2o decendio', '3o decendio']
 #available_rows_columns = df.columns
-#is_not_value = [True]*len(df.columns)
-#is_value = [False]*len(df.columns)
-is_value = [False]*len(available_variables)
-st.markdown("#### Valores")
-#if True not in is_value:
-st.markdown('Escolhe ao menos 1 atributo como variável.')
-for i in range(len(available_variables)):
-    #c = df.columns[i]
-    #is_not_value[i] = not st.checkbox(c, False)
-    #is_value[i] = not is_not_value[i]
-    is_value[i] = st.checkbox(available_variables[i], False)
 
-#available_rows_columns = list(compress(available_rows_columns, is_not_value))
-values = list(compress(available_variables, is_value))
-
-#value = st.selectbox('Valor usado', values)
-#value = '1o decendio'
+st.markdown(
+"""### Colunas e linhas
+Selecione quais dimensões serão utilizadas como colunas e quais como linhas""")
 #selected_columns = []
 #selected_rows = []
-
-st.markdown("#### Colunas e linhas")
 
 selected_columns = st.multiselect(
     "Colunas", available_rows_columns#list(df.columns)#, [df.columns[0]]
@@ -105,18 +100,56 @@ empty_complement = True
 
 complement = set(available_rows_columns)-set(selected_columns+selected_rows)
 if len(complement) > 0:
-    st.markdown("#### Atributos para escolher\n"+('  \n'.join(complement)))
+    st.markdown("##### Dimensões obrigatórias para escolher:\n"+('  \n'.join(complement)))
     empty_complement = False
 
 intersection = set(selected_columns).intersection(set(selected_rows))
 if len(intersection) > 0:
-    st.markdown(f"""#### Atributos não podem ser escolhidos como coluna e linha ao mesmo tempo
+    st.markdown(f"""##### Dimensões não podem ser escolhidos como coluna e linha ao mesmo tempo
 {', '.join(intersection)}
 """)
     exists_intersection = True
 
+#is_not_value = [True]*len(df.columns)
+#is_value = [False]*len(df.columns)
+is_value = [False]*len(available_variables)
+st.markdown("### Variável")
+#if True not in is_value:
+for i in range(len(available_variables)):
+    #c = df.columns[i]
+    #is_not_value[i] = not st.checkbox(c, False)
+    #is_value[i] = not is_not_value[i]
+    is_value[i] = st.checkbox(available_variables[i], True)
+
+#available_rows_columns = list(compress(available_rows_columns, is_not_value))
+values = list(compress(available_variables, is_value))
+variables_selected = True
+if len(values) == 0:
+    st.markdown('##### Escolhe ao menos 1 variável.')
+    st.text('')
+    variables_selected = False
+
+
+#value = st.selectbox('Valor usado', values)
+#value = '1o decendio'
+
+st.markdown("### Ano")
+anos = df['ano'].unique()#.sort()
+use_year = [False]*len(anos)
+for i in range(len(anos)):
+    use_year[i] = st.checkbox(str(anos[i]), True)
+#st.text(use_year)
+
+anos = list(compress(anos, use_year))
+years_selected = True
+if len(anos) == 0:
+    st.markdown('##### Escolhe ao menos 1 ano.')
+    st.text('')
+    years_selected = False
+
+df = df[df['ano'].isin(anos)]
     
-if not exists_intersection and empty_complement:
+if not exists_intersection and empty_complement and variables_selected and years_selected:
     #df = df[selected_columns+selected_rows+values]
     #df /= 1000000.0
     #df = df.T.reset_index()
@@ -200,13 +233,30 @@ if not exists_intersection and empty_complement:
         b64 = base64.b64encode(val)  # val looks like b'...'
         return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="Export_Orbita.xlsx">Baixar XLSX</a>' # decode b'abc' => abc
 
+
+    st.markdown('### Tabela customizada')
     if st.button('Gerar CSV'):
         st.markdown(get_table_download_link_csv(df_pivot), unsafe_allow_html=True)
     if st.button('Gerar XLSX'):
-        st.markdown(get_table_download_link_xlsx2(df_pivot), unsafe_allow_html=True)
+        within_limits = True
+        if df_pivot.shape[0] > 1048576:
+            within_limits = False
+            st.markdown('##### O número de linhas da tabela a ser exportada excede o limite suportado por Excel.')
+            st.text('')
+        if df_pivot.shape[1] > 16384:
+            within_limits = False
+            st.markdown('##### O número de colunas da tabela a ser exportada excede o limite suportado por Excel.')
+            st.text('')
+        
+        if within_limits:
+            st.markdown(get_table_download_link_xlsx2(df_pivot), unsafe_allow_html=True)
 
     #st.write("### Dados", df.head(20))
-    st.write("### Tabela customizada", df_pivot)#.head(50))
+    if st.button('Exibir tabela'):
+        st.write(df_pivot)
+    #st.write(f"""### Tabela customizada
+    #""", df_pivot)#.head(50))
+#Layout: 1 tabela [{max(1, len(selected_rows))} x {max(1, len(selected_columns))}] - {max(1, len(selected_rows)) * max(1, len(selected_columns))} valores
 
     # df = df.T.reset_index()
     # df = pd.melt(df, id_vars=["index"]).rename(
@@ -222,3 +272,6 @@ if not exists_intersection and empty_complement:
     #     )
     # )
     # st.altair_chart(chart, use_container_width=True)
+else:
+    st.markdown("""### Pendências
+Para gerar a tabela, favor resolver as pendências em sua configuração, observando as mensagens embaixo de cada seção.""")
